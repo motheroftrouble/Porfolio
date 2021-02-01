50bfb433c8561622b537a6d626819d7d5ffec33f
Paula Culler <paulajculler@gmail.com>
2021-01-31 21:46:26 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,6 +60,13 @@ $(document).ready(function() {
 
 	
 	});
+	
+	import { CountUp } from './js/countUp.min.js';
+
+		window.onload = function() {
+  			var countUp = new CountUp('target', 2000);
+  			countUp.start();
+		}
 
 
 });
